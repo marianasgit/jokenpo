cec10dca7e8c53d614d3ea1b326fa9a0bab153f1
Mariana Gomes <marianabicla@gmail.com>
2021-09-03 00:14:54 -0300

    acrescentando solução apresentada em sala

--- src/jokenpo/Jokenpo.java
+++ src/jokenpo/Jokenpo.java
@@ -11,8 +11,10 @@ public class Jokenpo {
 		
 		Random sorteio = new Random();
 		
-		int valorSorteado = sorteio.nextInt(3) + 1;
+		int valorComputador = sorteio.nextInt(3) + 1;
 		int valorJogador;
+		int pedra = 1, papel = 2, tesoura = 3;
+		
 		
 		System.out.println("******J O K E N P Ô******");
 		System.out.println();
@@ -27,41 +29,90 @@ public class Jokenpo {
 		System.out.println();
 		leitor.close();
 		
-		if (valorJogador == 1) {
-			System.out.println("Você escolheu: Pedra");
-		} else if (valorJogador == 2) {
-			System.out.println("Você escolheu: Papel");
-		} else {
-			System.out.println("Você escolheu: Tesoura");
+		String escolhaJogador, escolhaComputador;
+		
+		if (valorJogador == pedra) {
+			escolhaJogador = "Pedra"; 
+		} else if (valorJogador == papel) {
+			escolhaJogador = "Papel";
+		}else {
+			escolhaJogador = "Tesoura";
 		}
 		
-		if (valorSorteado == 1) {
-			System.out.println("A escolha do computador foi: Pedra");
-		} else if (valorSorteado == 2) {
-			System.out.println("A escolha do computador foi: Papel");
-		} else {
-			System.out.println("A escolha do computador foi: Tesoura");
+		if (valorComputador == pedra) {
+			escolhaComputador = "Pedra";
+		}else if (valorComputador == papel) {
+			escolhaComputador = "Papel";
+		}else {
+			escolhaComputador = "Tesoura";
 		}
+		
+		System.out.println("Você escolheu: " + escolhaJogador + "!");
+		System.out.println("O computador escolheu: " + escolhaComputador + "!");
+		
+		
+//		if (valorJogador == pedra) {
+//			System.out.println("Você escolheu: Pedra");
+//		} else if (valorJogador == papel) {
+//			System.out.println("Você escolheu: Papel");
+//		} else {
+//			System.out.println("Você escolheu: Tesoura");
+//		}
+//		
+//		if (valorComputador == pedra) {
+//			System.out.println("A escolha do computador foi: Pedra");
+//		} else if (valorComputador == papel) {
+//			System.out.println("A escolha do computador foi: Papel");
+//		} else {
+//			System.out.println("A escolha do computador foi: Tesoura");
+//		}
 		
 		System.out.println();
 		
-		if (valorSorteado == 1 && valorJogador == 2) {
-			System.out.println("Você venceu!");
-		} else if (valorSorteado == 1 && valorJogador == 3) {
-			System.out.println("O computador venceu!");
-		} else if (valorSorteado == 2 && valorJogador == 1) {
-			System.out.println("O computador venceu!");
-		} else if (valorSorteado == 2 && valorJogador == 3) {
-			System.out.println("Você venceu!");
-		} else if (valorSorteado == 3 && valorJogador == 1) {
-			System.out.println("Você venceu!");
-		} else if (valorSorteado == 3 && valorJogador == 2) {
-			System.out.println("O computador venceu!");
+//		if (valorComputador == pedra && valorJogador == papel) {
+//			System.out.println("Você venceu!");
+//		} else if (valorComputador == pedra && valorJogador == tesoura) {
+//			System.out.println("O computador venceu!");
+//		} else if (valorComputador == papel && valorJogador == pedra) {
+//			System.out.println("O computador venceu!");
+//		} else if (valorComputador == papel && valorJogador == tesoura) {
+//			System.out.println("Você venceu!");
+//		} else if (valorComputador == tesoura && valorJogador == pedra) {
+//			System.out.println("Você venceu!");
+//		} else if (valorComputador == tesoura && valorJogador == papel) {
+//			System.out.println("O computador venceu!");
+//		} else {
+//			System.out.println("Empate!");
+//		}
+		
+		
+		if (valorJogador == pedra) {
+			if (valorComputador == pedra) {
+				System.out.println("Empate!");
+			} else if (valorComputador == papel) {
+				System.out.println("O computador venceu!");
+			} else {
+				System.out.println("Você venceu!");
+			}
+				
+		} else if (valorJogador == papel) {
+			if (valorComputador == papel) {
+				System.out.println("Empate!");
+			} else if (valorComputador == pedra) {
+				System.out.println("Você venceu!");
+			} else {
+				System.out.println("O computador venceu!");
+			}
 		} else {
-			System.out.println("Empate!");
+			if (valorComputador == tesoura) {
+				System.out.println("Empate!");
+			} else if (valorComputador == pedra) {
+				System.out.println("O computador venceu!");
+			} else {
+				System.out.println("Você venceu!");
+			}
 		}
 		
- 
 	}
 
 }
